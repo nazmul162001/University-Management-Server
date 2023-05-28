cc93d37a321e52322906da38a9415d349eadc1e3
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-05-29 00:18:10 +0600

    setUp mongoose cors parser dotenv & database

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Connected to Mongo`)
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`Server connection error: ${error}`)
+  }
+}
+
+main()
