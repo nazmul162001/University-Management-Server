82ed4677e55c23d2b4c0bd15743b12bb063a0179
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-05-29 15:20:53 +0600

    added functionality for generated auto userId with decending order

--- src/app/modules/users/users.services.ts
+++ src/app/modules/users/users.services.ts
@@ -1,9 +1,16 @@
+import config from '../../../config'
 import { IUser } from './users.interface'
 import User from './users.model'
+import { generateUserId } from './users.utils'
 
 const createdUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated id
+  const id = await generateUserId()
+  user.id = id
   // default password
+  if (!user.password) {
+    user.password = config.default_user_password as string
+  }
 
   const createdUser = await User.create(user)
   if (!createdUser) {
