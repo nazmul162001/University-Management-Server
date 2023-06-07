3da5a9e65a468e41a8b6c6f9f2961ce87d072fca
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-06-07 08:05:20 +0600

    setUp error logs and success log with timeStamps with maximum days 14

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorLogger, logger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Connected to Mongo`)
+    logger.info(`Connected to Mongo`)
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(`Server connection error: ${error}`)
+    errorLogger.error(`Server connection error: ${error}`)
   }
 }
 
