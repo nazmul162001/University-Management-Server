fe4bc42dfb5d6851b8ba863f3fd74e7a0ce36399
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-06-14 11:28:49 +0600

    added query for semester page, limit, depend on date, year & code

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -5,6 +5,8 @@ import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemester } from './academicSemesterModel'
 import { IPaginationOptions } from '../../../interfaces/paginations'
 import { IGenericResponse } from '../../../interfaces/common'
+import { paginationHelper } from '../../../helpers/paginationHelper'
+import { SortOrder } from 'mongoose'
 
 const createSemester = async (
   payload: IAcademicSemester
@@ -20,9 +22,19 @@ const createSemester = async (
 const getAllSemesters = async (
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
-  const { page = 1, limit = 10 } = paginationOptions
-  const skip = (page - 1) * limit
-  const result = await AcademicSemester.find().sort().skip(skip).limit(limit)
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(paginationOptions)
+
+  const sortConditions: { [key: string]: SortOrder } = {}
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder
+  }
+
+  const result = await AcademicSemester.find()
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit)
 
   const total = await AcademicSemester.countDocuments()
 
